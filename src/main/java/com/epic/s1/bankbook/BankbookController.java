cd1a78c06c0c6a1a20588648f8579ca938f13ae8
JulyRam <wogjs715@naver.com>
2021-08-26 12:47:43 +0900

    .

--- src/main/java/com/epic/s1/bankbook/BankbookController.java
+++ src/main/java/com/epic/s1/bankbook/BankbookController.java
@@ -7,6 +7,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class BankbookController {
 	private BankbookDAO bankbookDAO;
@@ -101,6 +102,12 @@ public class BankbookController {
 			System.out.println(bankBookDTO.getBookName());
 			
 			request.setAttribute("dto", bankBookDTO);
+			request.setAttribute("count", 123);
+			request.setAttribute("name", "iu");
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("se", "session");
+			request.setAttribute("se", "request");
 			
 			RequestDispatcher view = request.getRequestDispatcher("../WEB-INF/views/bankbook/bankbookSelect.jsp");
 			try {
--- src/main/webapp/WEB-INF/views/bankbook/bankbookSelect.jsp
+++ src/main/webapp/WEB-INF/views/bankbook/bankbookSelect.jsp
@@ -10,13 +10,13 @@
 <body>
 	<h1>Bankbook Select Page</h1>
 	
-	<%
-		String num = request.getParameter("bookNumber");
-		Object obj = request.getAttribute("dto");
-		BankbookDTO bankbookDTO = (BankbookDTO)obj;
-	%>
 	
-	<h3>num : <%= num %></h3>
-	<h3>Name : <%= bankbookDTO.getBookName() %></h3>
+	
+	<h3>num : %{dto.bookNumber}</h3>
+	<h3>Name : ${requestScope.dto.bookName}</h3>
+	<h3>count : ${requestScope.count}</h3>
+	<h3>name : ${name}</h3>
+	<h3>se : ${sessionScape.se}</h3>
+	<h3>se : ${se}</h3>
 </body>
 </html>
